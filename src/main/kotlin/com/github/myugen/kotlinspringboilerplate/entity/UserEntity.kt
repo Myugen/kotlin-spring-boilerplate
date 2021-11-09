69c63e778f41e05ef194187f2aee92ab310bddbb
Miguel Cabrera <miguel.cabrera@system73.com>
2021-11-09 18:51:54 +0000

    feat: Add hibernate meta info to user entity

--- src/main/kotlin/com/github/myugen/kotlinspringboilerplate/entity/UserEntity.kt
+++ src/main/kotlin/com/github/myugen/kotlinspringboilerplate/entity/UserEntity.kt
@@ -1,20 +1,23 @@
 package com.github.myugen.kotlinspringboilerplate.entity
 
 import com.github.myugen.kotlinspringboilerplate.model.User
+import org.hibernate.annotations.Type
 import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.Table
 
 @Entity
+@Table(name = "users")
 class UserEntity(
-    val id: UUID?,
-    @Column(nullable = false) val username: String,
+    @Type(type = "pg-uuid") @Column(length = 36) val id: UUID?,
+    @Column(nullable = false, unique = true) val username: String,
     @Column(nullable = false) val name: String,
-    @Column(nullable = false) val mail: String
+    @Column(nullable = false, unique = true) val mail: String
 ) : JpaAuditable<UUID>(id) {
     companion object Factory {
         fun fromModel(model: User): UserEntity {
-            return UserEntity(model.id, model.username, model.name, model.email)
+            return UserEntity(model.id.id, model.username, model.name, model.email)
         }
     }
 }
