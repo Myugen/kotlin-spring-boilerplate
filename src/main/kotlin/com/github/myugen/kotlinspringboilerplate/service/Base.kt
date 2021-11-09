d30f2e09d2eb0204b6c1478d8fa07a23dd435de7
Miguel Cabrera <miguel.cabrera@system73.com>
2021-11-09 18:50:37 +0000

    refactor: Include the ID type on CrudService interface

--- src/main/kotlin/com/github/myugen/kotlinspringboilerplate/service/Base.kt
+++ src/main/kotlin/com/github/myugen/kotlinspringboilerplate/service/Base.kt
@@ -1,13 +1,12 @@
 package com.github.myugen.kotlinspringboilerplate.service
 
-import com.github.myugen.kotlinspringboilerplate.model.Identifiable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
-interface CrudUseCase<M : Identifiable> {
+interface CrudUseCase<M, ID> {
     fun create(model: M): M
     fun update(model: M): M
-    fun delete(identifiable: Identifiable)
+    fun delete(uid: ID)
     fun query(page: Pageable): Page<M>
-    fun find(identifiable: Identifiable): M
+    fun find(uid: ID): M
 }
--- src/main/kotlin/com/github/myugen/kotlinspringboilerplate/service/UserService.kt
+++ src/main/kotlin/com/github/myugen/kotlinspringboilerplate/service/UserService.kt
@@ -1,17 +1,16 @@
 package com.github.myugen.kotlinspringboilerplate.service
 
 import com.github.myugen.kotlinspringboilerplate.entity.UserEntity
-import com.github.myugen.kotlinspringboilerplate.model.Identifiable
 import com.github.myugen.kotlinspringboilerplate.model.User
 import com.github.myugen.kotlinspringboilerplate.repository.UserRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
-interface UserUseCase : CrudUseCase<User>
+interface UserUseCase : CrudUseCase<User, User.Id>
 
 @Service
-class UserService(val userRepository: UserRepository) : UserUseCase {
+class UserService(private val userRepository: UserRepository) : UserUseCase {
     override fun create(model: User): User {
         return save(model)
     }
@@ -20,16 +19,16 @@ class UserService(val userRepository: UserRepository) : UserUseCase {
         return save(model)
     }
 
-    override fun delete(identifiable: Identifiable) {
-        userRepository.deleteById(identifiable.id!!)
+    override fun delete(uid: User.Id) {
+        userRepository.deleteById(uid.id!!)
     }
 
     override fun query(page: Pageable): Page<User> {
         return userRepository.findAll(page).map(User.Factory::fromEntity)
     }
 
-    override fun find(identifiable: Identifiable): User {
-        val entity = userRepository.findById(identifiable.id!!).get()
+    override fun find(uid: User.Id): User {
+        val entity = userRepository.findById(uid.id!!).get()
         return User.fromEntity(entity)
     }
 
